1a97a04bfb8cc3146b8f5d045683c79892231884
Eric Dasse <christ.gououi@gmail.com>
2023-11-02 22:46:40 +0100

    create_tables: use `make_sqlalchemy_db_url`

--- kata_data_foodies/create_tables.py
+++ kata_data_foodies/create_tables.py
@@ -1,16 +1,18 @@
 """Create tables in the database."""
 
 
+from dotenv import load_dotenv
 from loguru import logger
 from sqlalchemy import create_engine
-from kata_data_foodies import config
 from kata_data_foodies.models import metadata
+from kata_data_foodies.utils import make_sqlalchemy_db_url
 
 if __name__ == "__main__":
+    load_dotenv()
+
     logger.info("Creating database...")
 
-    db_url = f"postgresql+psycopg2://{config.DB_USER}:{config.DB_PASSWORD}@\
-{config.DB_HOST}:{config.DB_PORT}/recipe_db"
+    db_url = make_sqlalchemy_db_url()
     engine = create_engine(db_url)
     metadata.create_all(bind=engine)
 
